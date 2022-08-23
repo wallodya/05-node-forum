430eda10cd44b270334012f917f7b9a1b722cf11
wallodya <vixlyaev2000@gmail.com>
2022-08-23 21:10:43 +0000

    new title

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const createPage = (content) => {
                     <meta charset="UTF-8">
                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Node test</title>
+                    <title>Souta</title>
                     <link rel="stylesheet" type="text/css" href="/css/style.css">
                     <link rel="stylesheet" type="text/css" href="/css/city.css">
                 </head>
