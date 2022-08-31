15144cae931e151c412dd996965477deb526792e
wallodya <vixlyaev2000@gmail.com>
2022-08-31 16:26:06 +0000

    form

--- app.js
+++ app.js
@@ -5,13 +5,32 @@ const http = require('http')
 const express = require('express')
 const res = require('express/lib/response')
 const fs = require('fs')
-// const bodyParser =require('body-parser')
 const urlencodedParser = express.urlencoded({extended: false})
+const cookieParser = require('cookie-parser')
+const sessions = require('express-session')
 const app = express()
 
 app.use(express.static('./public'))
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+const msInDay = 24 * 60 * 60 * 1000
+app.use(sessions ({
+    secret: "secretkey",
+    saveUninitialized: true,
+    cookie: {maxAge: msInDay},
+    resave: false
+}))
+let session
+app.use(cookieParser())
+
+// const handlebars = require('express-handlebars')
+
+// app.engine('handlebars', () => {handlebars()})
+
+// app.set('views', './html')
+// app.set('view engine', 'handlebars')
 
 
 let header = ''
@@ -19,33 +38,40 @@ let footer = ''
 const PAGES = require('./pages.json')
 const { status } = require('express/lib/response')
 
+
 let messageCounter = 0
+
+// let isUser = false
+// let userName = ''
 
 // Getting requests for images 
 const imageRegex = new RegExp('^\/[A-z0-9]+\.((png)|(webp)|(jpg))$')
 
-app.get(imageRegex, (req, res) => {
-    res.status(200)
-    fs.readFile(`./public/img${req.url}`, (err, data) => {
-        if (err) {
-            res.send('<h1>No such image</h1>')
-        } else{
-            res.send(`<img src="/img${req.url}" alt="some image">`)
-        }
-    })
-})
-
-//Getting requests for urls
-for (let address in PAGES) {
+const getPage = (address) => {
     app.get(address, (req, res) => {
-        res.status(200)
+
+        let isUser = false
+        
+        session = req.session
+
+        if (session.userid) {
+            isUser = true
+        }
+
+        let scripts = ''
+        if (isUser) {
+            header = fs.readFileSync('./public/html/header_user.html')
+            scripts += `<script defer>const userName = "${session.userid}"</script>`
+        } else {
+            header = fs.readFileSync('./public/html/header_guest.html')
+        }
 
 
-        // Generating header
-        fs.readFile(PAGES[address]["header"], (err, data) =>{
-            if (err) {throw err}
-            header = data
-        })
+        // console.log(address, 1)
+        // if (!isUser && PAGES[address]["userOnly"]) {    
+        //     res.redirect(301, '/')
+        // }
+        // console.log(address, 2)
 
         // Generating footer
         if (PAGES[address]["bigFooter"]) {
@@ -60,18 +86,16 @@ for (let address in PAGES) {
             })
         }
 
-        //Generating list for required scripts
-        let scripts = ''
+        //Generating tags for required scripts
         for (let i = 0; i < PAGES[address]["scripts"].length; i++) {
             scripts += `<script src="${PAGES[address]["scripts"][i]}" defer></script>\n`
         }
 
-        //Generating list for required styles
+        //Generating tags for required styles
         let styles = ''
         for (let i = 0; i < PAGES[address]["styles"].length; i++) {
             styles += `<link rel="stylesheet" type="text/css" href="${PAGES[address]["styles"][i]}">\n`
         }
-        console.log(styles)
 
         // Generating page layout
         fs.readFile(PAGES[address]["content"], (err, data) => {
@@ -94,12 +118,40 @@ for (let address in PAGES) {
                 ${footer}
                 </body
                 </html>`
-            console.log('sending', address)
-            res.send(page)
+            // console.log('sending', address)
+            if (!isUser && PAGES[address]["userOnly"]) {
+                res.redirect(301, '/home')
+            } else {
+                res.send(page)
+            }
         })
     })
 }
 
+app.get(imageRegex, (req, res) => {
+    res.status(200)
+    fs.readFile(`./public/img${req.url}`, (err, data) => {
+        if (err) {
+            res.send('<h1>No such image</h1>')
+        } else{
+            res.send(`<img src="/img${req.url}" alt="some image">`)
+        }
+    })
+})
+
+app.get('/logout', (req, res) => {
+    console.log('logout')
+    req.session.destroy()
+    res.redirect(req.get('referer'))
+    console.log('logged out')
+})
+
+//Getting requests for urls
+for (let address in PAGES) {
+        getPage(address)
+}
+
+// Managing post request from forum
 app.post('/message', urlencodedParser, (req, res) => {
     res.sendStatus(200)
     let message = req.body
@@ -117,22 +169,25 @@ app.post('/message', urlencodedParser, (req, res) => {
     })
 })
 
+//Managing login post requests
 app.post('/login', urlencodedParser, (req, res) => {
     const clientLogin = req.body.login
     const clientPassword = req.body.password
+
     console.log(clientLogin, clientPassword)
 
     let users = fs.readFileSync('./users.json')
     users = JSON.parse(users)
-    if (!users.hasOwnProperty(clientLogin)) {
-        res.sendStatus(400)
-    } else if (clientPassword != users[clientLogin].password) {
-        res.sendStatus(401)
-    } else {
-        res.sendStatus(200)
-    }
 
-    // console.log(users)
+    if (users.hasOwnProperty(clientLogin) && users[clientLogin].password == clientPassword) {
+        session = req.session
+        session.userid = clientLogin
+        console.log(req.session)
+        res.redirect(req.get('referer'))
+        console.log(200)
+    } else {
+        res.sendStatus(401)
+    }
 })
 
 app.listen(port, host, () => {
--- public/js/login.js
+++ public/js/login.js
@@ -1,182 +1,217 @@
 
 const loginButton = document.getElementById('login-button')
-const sendButtons = document.getElementsByClassName('send-button')
-const loginField = document.getElementById('login-field')
-const passwordField = document.getElementById('password-field')
+const logoutButton = document.getElementById('logout-button')
+// const sendButtons = document.getElementsByClassName('send-button')
+const submitButton = document.getElementById('submit-button')
+// const loginField = document.getElementById('login-field')
+// const passwordField = document.getElementById('password-field')
 const popupContainer = document.getElementById('popup-background')
-const popup = document.getElementById('popup-login')
-const wrongLoginText =document.getElementById('wrong-login-text')
-const wrongPasswordText =document.getElementById('wrong-password-text')
-const correctLoginText =document.getElementById('correct-login-text')
-const correctPasswordText =document.getElementById('correct-password-text')
-let isLoginValid = false
-let isPasswordValid = false
+// const popup = document.getElementById('popup-login')
+// const wrongLoginText =document.getElementById('wrong-login-text')
+// const wrongPasswordText =document.getElementById('wrong-password-text')
+// const correctLoginText =document.getElementById('correct-login-text')
+// const correctPasswordText =document.getElementById('correct-password-text')
+// let isLoginValid = false
+// let isPasswordValid = false
 
-//Assigning 'wrong-input' class to input field and showing/hiding warning labels
-//depending on isIncorrect flag and changing the isLoginPossible flag
-markIncorrectField = (inputField, isIncorrect) => {
-    switch (isIncorrect) {
-        case true: {
-            inputField.classList.add('wrong-input')
+try {
+    if (userName) {
+        console.log(userName + '...')
+        document.getElementById('username-field').innerText = `Your login is ${userName}`
+    }
+} catch (err) {
+    console.log(err)
+}
 
-            switch (inputField){ 
-                case loginField: {
-                    isLoginValid = false
-                    wrongLoginText.style.height = 'fit-content'
-                    break
-                }
-                case passwordField: {
-                    isPasswordValid = false
-                    wrongPasswordText.style.height = 'fit-content'
-                    break
-                }
-            }  
+
+
+// //Assigning 'wrong-input' class to input field and showing/hiding warning labels
+// //depending on isIncorrect flag and changing the isLoginPossible flag
+// markIncorrectField = (inputField, isIncorrect) => {
+//     switch (isIncorrect) {
+//         case true: {
+//             inputField.classList.add('wrong-input')
+
+//             switch (inputField){ 
+//                 case loginField: {
+//                     isLoginValid = false
+//                     wrongLoginText.style.height = 'fit-content'
+//                     break
+//                 }
+//                 case passwordField: {
+//                     isPasswordValid = false
+//                     wrongPasswordText.style.height = 'fit-content'
+//                     break
+//                 }
+//             }  
             
-            break
-        }
-        case false: {
-            inputField.classList.remove('wrong-input')
+//             break
+//         }
+//         case false: {
+//             inputField.classList.remove('wrong-input')
 
-            switch (inputField){ 
-                case loginField: {
-                    isLoginValid = true
-                    wrongLoginText.style.height = '0'
-                    break
-                }
-                case passwordField: {
-                    isPasswordValid = true
-                    wrongPasswordText.style.height = '0'
-                    break
-                }
-            }  
+//             switch (inputField){ 
+//                 case loginField: {
+//                     isLoginValid = true
+//                     wrongLoginText.style.height = '0'
+//                     break
+//                 }
+//                 case passwordField: {
+//                     isPasswordValid = true
+//                     wrongPasswordText.style.height = '0'
+//                     break
+//                 }
+//             }  
 
-            break
-        }
-    }      
-}
+//             break
+//         }
+//     }      
+// }
 
 
-// Cheking the value inside the input field with RegEx
-// and callinng markIncorrectField function in case its invalid
-checkFieldValue = (inputField) => {
-    const loginRegEx = new RegExp('^[A-z][A-z0-9.-]{3,19}$')
-    const passwordRegEx = new RegExp('^[A-z0-9]{4,19}$')
+// // Cheking the value inside the input field with RegEx
+// // and callinng markIncorrectField function in case its invalid
+// checkFieldValue = (inputField) => {
+//     const loginRegEx = new RegExp('^[A-z][A-z0-9.-]{3,19}$')
+//     const passwordRegEx = new RegExp('^[A-z0-9]{4,19}$')
 
-    switch (inputField) {
-        case loginField: {
-            if (!loginRegEx.test(loginField.value)) {
-                markIncorrectField(loginField, true)
-            } else {
-                markIncorrectField(loginField, false)
-            }
-            break
-        }
+//     switch (inputField) {
+//         case loginField: {
+//             if (!loginRegEx.test(loginField.value)) {
+//                 markIncorrectField(loginField, true)
+//             } else {
+//                 markIncorrectField(loginField, false)
+//             }
+//             break
+//         }
 
-        case passwordField: {
-            if (!passwordRegEx.test(passwordField.value)) {
-                markIncorrectField(passwordField, true)
-            } else {
-                markIncorrectField(passwordField, false)
-            }
-            break
-        }
-    }
-} 
+//         case passwordField: {
+//             if (!passwordRegEx.test(passwordField.value)) {
+//                 markIncorrectField(passwordField, true)
+//             } else {
+//                 markIncorrectField(passwordField, false)
+//             }
+//             break
+//         }
+//     }
+// } 
 
-const sendData = async () => {
-        console.log('sending data')
-        userInput = {
-            login : loginField.value,
-            password : passwordField.value
-        }
+// const sendData = async () => {
+//         console.log('sending data')
+//         userInput = {
+//             login : loginField.value,
+//             password : passwordField.value 
+//         }
         
-        const jsonBody = {   
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body : JSON.stringify(userInput)
-        }
-        console.log(jsonBody)
-        try {
-           const res = await fetch('/login', jsonBody)
-           console.log(res.status)
-           switch (res.status) {
-               case 400: {
-                   console.log('no such user')
-                   wrongLoginText.innerText = 'User doesn`t exist'
-                   markIncorrectField(loginField, true)
-                   break
-               }
-               case 401: {
-                   console.log('Wrong password')
-                   wrongPasswordText.innerText = 'Wrong password'
-                   markIncorrectField(passwordField, true) 
-                   break
-               }
-               case 200: {
-                   loginField.classList.add('correct-input')
-                   passwordField.classList.add('correct-input')
-                   correctLoginText.style.height = 'fit-content'
-                   correctPasswordText.style.height = 'fit-content'
-               }
-           }
-        } catch (err) {
-        }
-}
+//         const jsonBody = {   
+//             method: 'POST',
+//             headers: {'Content-Type': 'application/json'},
+//             body : JSON.stringify(userInput)
+//         }
+//         console.log(jsonBody)
+//         try {
+//            const res = await fetch('/login', jsonBody)
+//            console.log('sending data')
+//            console.log(res.status)
+//            switch (res.status) {
+//                case 401: {
+//                     wrongLoginText.innerText = 'Authentification failed'
+//                     wrongPasswordText.innerText = 'Authentification failed'
+//                     markIncorrectField(passwordField, true) 
+//                     markIncorrectField(loginField, true)
+//                     break
+//                }
+//                case 200: {
+//                     console.log(200)
+//                     loginField.classList.add('correct-input')
+//                     passwordField.classList.add('correct-input')
+//                     correctLoginText.style.height = 'fit-content'
+//                     correctPasswordText.style.height = 'fit-content'
+//                     setTimeout(() => {
+//                         closePopup()
+//                     }, 750)
+//                     setTimeout(() => {
+//                         location.reload()
+//                     }, 1000)
+                
+                   
+//                 //    setTimeout(() => {reloadPage(true)}, 750)
+//                }
+//            }
+//         } catch (err) {
+//         }
+// }
 
-const setDefaultLoginField = () => {
-    loginField.setAttribute('class', 'neomorph')
-    correctLoginText.style.height = '0'
-    wrongLoginText.innerText = 'Login incorrect'
-    wrongLoginText.style.height = '0'
-}
+// const setDefaultLoginField = () => {
+//     loginField.setAttribute('class', 'neomorph')
+//     correctLoginText.style.height = '0'
+//     wrongLoginText.innerText = 'Login incorrect'
+//     wrongLoginText.style.height = '0'
+// }
 
-const setDefaultPasswordField = () => {
-    passwordField.setAttribute('class', 'neomorph')
-    correctPasswordText.style.height = '0'
-    wrongPasswordText.innerText = 'Wrong password format'
-    wrongPasswordText.style.height = '0'
-}
+// const setDefaultPasswordField = () => {
+//     passwordField.setAttribute('class', 'neomorph')
+//     correctPasswordText.style.height = '0'
+//     wrongPasswordText.innerText = 'Wrong password format'
+//     wrongPasswordText.style.height = '0'
+// }
 
-for (let i = 0; i < sendButtons.length; i++) {
-    sendButtons[i].onclick = () => {
-        checkFieldValue(loginField)
-        checkFieldValue(passwordField)
-        if (isLoginValid && isPasswordValid) {
-            sendData()
-        }
+// const closePopup = () => {
+//     popupContainer.style.display ='none'
+//     loginField.value = ''
+//     passwordField.value = ''
+//     setDefaultLoginField()
+//     setDefaultPasswordField()
+// }
+
+
+// for (let i = 0; i < sendButtons.length; i++) {
+//     sendButtons[i].onclick = () => {
+//         checkFieldValue(loginField)
+//         checkFieldValue(passwordField)
+//         if (isLoginValid && isPasswordValid) {
+//             sendData()
+//         }
+//     }
+// }
+
+if (loginButton) {
+    loginButton.onclick = () => {
+        popupContainer.style.display = 'block'
+    }
+}
+if (logoutButton) {
+    logoutButton.onclick = async () => {
+        console.log('logout')
+        await fetch('http://forum.souta.ru/logout').then(() => {
+            location.reload()
+        })
     }
 }
 
-loginButton.onclick = () => {
-    popupContainer.style.display = 'block'
-}
 
-loginField.addEventListener('input', () => {
-    setDefaultLoginField()
-})
+// loginField.addEventListener('input', () => {
+//     setDefaultLoginField()
+// })
 
-passwordField.addEventListener('input', () => {
-    setDefaultPasswordField()
-})
+// passwordField.addEventListener('input', () => {
+//     setDefaultPasswordField()
+// })
 
-loginField.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        sendData()
-    } 
-})
+// loginField.addEventListener('keypress', (event) => {
+//     if (event.key === 'Enter') {
+//         sendData()
+//     } 
+// })
 
-passwordField.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        sendData()
-    } 
-})
+// passwordField.addEventListener('keypress', (event) => {
+//     if (event.key === 'Enter') {
+//         sendData()
+//     } 
+// })
 
-window.onclick = (event) => {
-    if (event.target == popupContainer) {
-        popupContainer.style.display ='none'
-        loginField.value = ''
-        passwordField.value = ''
-        setDefaultLoginField()
-        setDefaultPasswordField()
-    }
-}
+// window.onclick = (event) => {
+//     if (event.target == popupContainer) {
+//         closePopup()
+//     }
+// }
